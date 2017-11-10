df6a4666019afd7a8d40dd4cb782834134c92ef3
carmonc <carmonc@sunyit.edu>
2017-11-10 12:10:17 -0500

    Append HTTP submission data to a file GNUPlot support Submit functionality complete Reporting functionality installed Dynamic point file generation Delete old submission data @ startup URL 'report' displays list of submitted data

--- spwebserver.js
+++ spwebserver.js
@@ -1,20 +1,64 @@
 var fs = require('fs');
+var os = require("os");
 var net = require('net');
 var util = require('util');
 var sleep = require('sleep');
 var express = require('express');
+var gnuplot = require('gnuplot');
 var program = require('commander');     //Command line parsing module
 var scribe = require('scribe-js')();
 var waitSync = require('wait-sync');
 var bodyParser = require('body-parser');
+var LineByLineReader = require("line-by-line");
 
+/**
+ * History
+ * 0.0.1 - Initial Import and basic web functionality (didn't really do anything, though)
+ * 0.7.0 - Added multiple features
+ *          - Append HTTP submission data to a file
+ *          - GNUPlot support
+ *          - Submit functionality complete
+ *          - Reporting functionality installed
+ *          - Dynamic point file generation
+ *          - Delete old submission data @ startup
+ *          - URL 'report' displays list of submitted data
+ * 
+ */
+
+/**
+ * Command line options
+ */
 program
-    .version('0.0.1')
+    .version('0.7.0')
     .option('-p, --port [portNum]','Sets the port for the webserver to listen on. [Default: 7100]',7100)
     .option('-w, --web-server-ip [webServerIp]','Binds the webserver to a specific IP Address','localhost')
     .parse(process.argv);
 
+
 var webApp = express();
+
+var count = 0;
+
+fs.exists("./submissions.p", function(exists) {
+   if(exists) {
+      console.log("Deleting previous submissions....");
+      fs.unlink("./submissions.p");
+      waitSync(0.5);
+      fs.writeFile("./submissions.p", "# Entry(1), Value(2)\n", function(err) {
+         if(err) {
+            return console.log(err);
+         }
+      });
+   }
+});
+
+//Is this a duplicate (from the above)? Required? FIXME!
+fs.writeFile("./submissions.p", "# Entry(1), Value(2)\n", function(err) {
+   if(err) {
+      return console.log(err);
+   }
+   waitSync(0.5);
+});
 
 webApp.use(bodyParser.urlencoded({
    extended:false
@@ -30,25 +74,36 @@ console.log("ipaddy = " + program.webServerIp);
 webApp.get("/", function(req, res) {
    console.log("Root hit!");
 
-   var page =" <!DOCTYPE html>\n<html>\n<body>\n<h1>Senior Project - Webserver</h1>\n<p>This webserver serves two URL - 'submit' and '/'.</p><p>Submit accepts a simple HTTP POST command with associated JSON data. The data is then written to a flat file for some future analysis.</p>\n</body>\n</html>"; 
-
-
+  var page ="<!DOCTYPE html>\n<html>\n<body>\n<h1>Senior Project - Webserver</h1>\n<p>This webserver serves two URL - 'submit' and '/'.</p><p>Submit accepts a simple HTTP POST command with associated JSON data. The data is then written to a flat file for some future analysis.</p>\n</body>\n</html>"; 
 
    res.type('html');
    res.send(page);
 });
 
 webApp.post("/submit", function(req, res) {
-   console.log("submit hit!");
-//	console.log(util.inspect( req.body));
+   console.log("Entering submission!");
 
-   var input = req.body;
-   input.replace(/\s+/g,' ').trim();
+   var input = req.body; 
+
    console.log(util.inspect(input));
-   fs.appendFile('submissions.txt', util.inspect(input), (err) => {
-      if(err)
-        throw error;
-      console.log('log updated');
+   var date = input.date;
+   var level = input.level;
+   var name = input.sensor;
+   input.date = date.replace(/\s+/g,'');
+   console.log("date = " + date);
+
+   //Append new input dat
+   fs.appendFile('./jsonreport.txt', util.inspect(input) + os.EOL, (err) => {
+   if(err)
+      throw error;
+      console.log('json report updated');
+   });
+
+   //Append to point file
+   fs.appendFile('./submissions.p', (++count) + " " + level + os.EOL, (err) => {
+   if(err)
+      throw error;
+      console.log('gnuplot point file updated');
    });
 
    res.type('json');
@@ -56,7 +111,54 @@ webApp.post("/submit", function(req, res) {
 
 });
 
+webApp.get("/report", function(req,res) {
 
+   console.log("Generating Report!");
+
+   gnuplot()
+      .set('term png small size 1024,768')
+      .set('output "./gnuplot.png"')
+      .set('title "sensor over time"')
+      .set('xlabel "Entry #"')
+      .set('xtics 1')
+      .set('ylabel "sensor level"')
+      .set('xrange []')
+      .set('yrange []')
+      .set('grid')
+      .set('mytics 5')
+      .set('style data linespoints')
+      .plot('"./submissions.p" lt 1 title "Sensor over time"')
+      .end();
+
+   var page ="\n\n<!DOCTYPE html>\n<html>\n<body>\n<h1>Senior Project - Sensor Over Time Report</h1>\n";
+   var graph = '\n\t\t<img src="gnuplot.png" alt="Sensor Data over time" height="768" width="1024">\n'; 
+   var tail = "\n</body>\n</html>\n";
+
+   page = page + graph;
+
+   fs.exists("./jsonreport.txt", function(exists) {
+      if(exists) {
+
+         lr = new LineByLineReader('./jsonreport.txt');
+
+         lr.on('line', function(line) { 
+            var newline = "\n<p>" + line + "</p>";
+            page = page + newline;
+         });
+
+         lr.on('end',function () {
+            page = page + tail;
+         });
+  
+      } else {
+         page = "\n\n<!DOCTYPE html>\n<html>\n<body>\n<h1>Senior Project - Sensor Over Time Report</h1>\n<p>No submissions at this time to address.</p></body>\n</html>\n";
+      }
+   });
+
+   waitSync(1.0);
+   res.type('html');
+   res.send(page);
+});
 
 
 
--- submit.sh
+++ submit.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -X POST -H "Content-Type: application/json" -d @submit.json http://localhost:7100/submit
+SUBMISSION_TIME=$(date +"%Y-%m-%dT%H:%M:%S")
+NUMBER=$(cat /dev/urandom | tr -dc '0-9' | fold -w 256 | head -n 1 | sed -e 's/^0*//' | head --bytes 2)
+curl -X POST -H "Content-Type: application/json" -d '{ "sensor":"GEN","level":"'"$NUMBER"'", "date":"'"$SUBMISSION_TIME"'"}' http://localhost:7100/submit
