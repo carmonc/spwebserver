6d1db07c8c4ad26a12b322a7fcde19a9b2b35fd0
carmoca <carmoca@gmail.com>
2017-11-10 16:23:30 -0500

    Initial import of module installation script

---
+++ install_modules.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+npm install util
+npm install sleep
+npm install express
+npm install gnuplot
+npm install commander
+npm install scribe-js
+npm install wait-sync
+npm install body-parser
+npm install line-by-line
+
